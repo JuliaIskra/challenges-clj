78096f11c82f5b9398b59415ad69ab32966cb099
Julia Nemtseva <juliaiskra@gmail.com>
2017-02-12 18:43:24 +0300

    Solve problem #1

--- project-euler/src/project_euler/core.clj
+++ project-euler/src/project_euler/core.clj
@@ -2,8 +2,13 @@
   (:gen-class))
 
 (defn problem1
+  "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+  The sum of these multiples is 23.
+  Find the sum of all the multiples of 3 or 5 below 1000."
   []
-  "hey!")
+  (->> (range 1000)
+       (filter (fn [n] (or (zero? (mod n 3)) (zero? (mod n 5)))))
+       (reduce +)))
 
 (defn -main
   [number & args]

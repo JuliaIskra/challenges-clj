19a7d37fac9fe9ccda50a6e85dc11c1317e8d554
Julia Nemtseva <juliaiskra@gmail.com>
2017-02-24 16:58:53 +0300

    Solve problem #3

--- project-euler/src/project_euler/core.clj
+++ project-euler/src/project_euler/core.clj
@@ -33,19 +33,24 @@
        (reduce +)))
 
 
-(defn is-prime?
+(defn calc-min-factor
   [n]
   (->> (range 2 n)
-       (map (partial mod n))
-       (filter zero?)
-       empty?))
+       (map (fn [div] [div (mod n div)]))
+       (filter (fn [[div rem]] (zero? rem)))
+       first
+       first))
 
 (defn problem3
   "https://projecteuler.net/problem=3
   The prime factors of 13195 are 5, 7, 13 and 29.
   What is the largest prime factor of the number 600851475143 ?"
   []
-  )
+  (loop [n 600851475143]
+    (let [min-factor (calc-min-factor n)]
+      (if (nil? min-factor)
+        n
+        (recur (/ n min-factor))))))
 
 
 (defn -main

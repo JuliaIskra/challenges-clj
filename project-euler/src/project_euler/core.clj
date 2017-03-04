917196d97998fd0f497c0d9f8d2894a4d65a6951
Julia Nemtseva <juliaiskra@gmail.com>
2017-03-04 19:05:10 +0300

    Solve problem #6

--- project-euler/src/project_euler/core.clj
+++ project-euler/src/project_euler/core.clj
@@ -106,6 +106,20 @@
        (reduce (fn [result [n mult]] (* result (exp n mult))) 1)))
 
 
+(defn problem6
+  "The sum of the squares of the first ten natural numbers is,
+      1^2 + 2^2 + ... + 10^2 = 385
+  The square of the sum of the first ten natural numbers is,
+      (1 + 2 + ... + 10)^2 = 552 = 3025
+  Hence the difference between the sum of the squares of the first ten natural numbers
+  and the square of the sum is 3025 − 385 = 2640.
+  Find the difference between the sum of the squares of the first one hundred natural numbers
+  and the square of the sum."
+  []
+  (- (exp (reduce + (range 1 101)) 2)
+     (reduce + (map #(exp %1 2) (range 1 101)))))
+
+
 (defn -main
   [number & args]
   (println (str "Running solution for problem #" number))

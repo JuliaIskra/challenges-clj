55b6c92784b99e9fbf8d041a72c83d3e33221829
Julia Nemtseva <juliaiskra@gmail.com>
2017-03-04 18:49:38 +0300

    Solve problem #5

--- project-euler/src/project_euler/core.clj
+++ project-euler/src/project_euler/core.clj
@@ -33,7 +33,7 @@
        (reduce +)))
 
 
-(defn calc-min-factor
+(defn calc-min-prime-factor
   [n]
   (->> (range 2 n)
        (map (fn [div] [div (mod n div)]))
@@ -47,7 +47,7 @@
   What is the largest prime factor of the number 600851475143 ?"
   []
   (loop [n 600851475143]
-    (let [min-factor (calc-min-factor n)]
+    (let [min-factor (calc-min-prime-factor n)]
       (if (nil? min-factor)
         n
         (recur (/ n min-factor))))))
@@ -78,6 +78,32 @@
        (filter #(not (nil? %1)))
        (sort #(compare %2 %1))
        first))
+
+
+(defn calc-prime-factors
+  [num]
+  (loop [n             num
+         prime-factors []]
+    (let [min-factor (calc-min-prime-factor n)]
+      (if (nil? min-factor)
+        (concat prime-factors [n])
+        (recur (/ n min-factor) (concat prime-factors [min-factor]))))))
+
+(defn exp
+  [n exp]
+  (reduce * (repeat exp n)))
+
+(defn problem5
+  "https://projecteuler.net/problem=5
+  2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+  What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?"
+  []
+  (->> (range 1 21)
+       (map calc-prime-factors)
+       (map frequencies)
+       (reduce #(merge-with max %1 %2) {})
+       seq
+       (reduce (fn [result [n mult]] (* result (exp n mult))) 1)))
 
 
 (defn -main

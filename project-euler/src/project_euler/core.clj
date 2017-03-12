603a36d9fd39310172f8939decf810506f00d232
Julia Nemtseva <juliaiskra@gmail.com>
2017-03-12 21:55:23 +0300

    Solve problem #7

--- project-euler/src/project_euler/core.clj
+++ project-euler/src/project_euler/core.clj
@@ -120,6 +120,27 @@
      (reduce + (map #(exp %1 2) (range 1 101)))))
 
 
+(defn problem7
+  "By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+  What is the 10001st prime number?"
+  []
+  (loop [prime-numbers []
+         count         0
+         next          2]
+    (if (== 10001 count)
+      (last prime-numbers)
+      (if (->> prime-numbers
+               (map #(mod next %1))
+               (filter zero?)
+               empty?)
+        (do (recur (concat prime-numbers [next])
+                   (inc count)
+                   (inc next)))
+        (do (recur prime-numbers
+                   count
+                   (inc next)))))))
+
+
 (defn -main
   [number & args]
   (println (str "Running solution for problem #" number))

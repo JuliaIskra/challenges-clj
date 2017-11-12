6802c59bc88c46b7335a95804adb65acaa2f48e8
Julia Nemtseva <juliaiskra@gmail.com>
2017-11-12 15:27:46 +0300

    Solve problem #15 by brute-force algo. It takes too much time to calculate the answer for now.

--- project-euler/src/project_euler/problem15.clj
+++ project-euler/src/project_euler/problem15.clj
@@ -1,14 +1,29 @@
 (ns project-euler.problem15)
 
-; 20x20 grid coordinates: 0x0..19x19
+(def grid 20)
 
 (defn can-go-right?
   [[x y]]
-  (< (inc x) 20))
+  (<= (inc x) grid))
 
 (defn can-go-down?
   [[x y]]
-  (< (inc y) 20))
+  (<= (inc y) grid))
+
+(defn go-right
+  [[x y]]
+  [(inc x) y])
+
+(defn go-down
+  [[x y]]
+  [x (inc y)])
+
+(defn next-steps
+  [coord]
+  (if-not (or (can-go-right? coord) (can-go-down? coord))
+    [coord]
+    [(if (can-go-right? coord) (go-right coord))
+     (if (can-go-down? coord) (go-down coord))]))
 
 (defn problem15
   "https://projecteuler.net/problem=15
@@ -16,4 +31,11 @@
   to the right and down, there are exactly 6 routes to the bottom right corner.
   How many such routes are there through a 20×20 grid?"
   []
-  )
+  (loop [routes    [[0 0]]
+         prev-size 0]
+    (if (= prev-size (count routes))
+      prev-size
+      (recur (->> routes
+                  (mapcat next-steps)
+                  (filter (comp not nil?)))
+             (count routes)))))

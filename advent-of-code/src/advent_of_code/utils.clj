054582e945bfc71b8b47a8b8796a24163833699c
Julia Nemtseva <juliaiskra@gmail.com>
2025-01-12 18:06:03 +0200

    refactor day 3

--- advent-of-code/src/advent_of_code/utils.clj
+++ advent-of-code/src/advent_of_code/utils.clj
@@ -1,5 +1,13 @@
 (ns advent-of-code.utils)
 
+(defn str->int
+  [n]
+  (Integer/parseInt n))
+
 (defn to-int-each
   [coll]
-  (map #(Integer/parseInt %) coll))
+  (map #(str->int %) coll))
+
+(defn ->>nth
+  [index col]
+  (nth col index))
--- advent-of-code/src/advent_of_code/y2024/day3.clj
+++ advent-of-code/src/advent_of_code/y2024/day3.clj
@@ -5,25 +5,18 @@
   [filename]
   (->>
     (slurp filename)
-    (re-seq #"mul\(\d{1,3},\d{1,3}\)")
-    (map #(re-seq #"\d+" %))
-    (map to-int-each)
-    (map #(reduce * %))
+    (re-seq #"mul\((\d{1,3}),(\d{1,3})\)")
+    (map (fn [[_ a b]] (* (str->int a) (str->int b))))
     (reduce +)))
 
 
-(defn collect-numbers
-  [input]
-  (loop [i 0
-         col []
-         save true]
-    (if (= i (count input))
-      col
-      (cond
-        (= (nth input i) "do()") (recur (inc i) col true)
-        (= (nth input i) "don't()") (recur (inc i) col false)
-        (true? save) (recur (inc i) (conj col (nth input i)) save)
-        :else (recur (inc i) col save)))))
+(defn save-muls
+  [[save muls] current]
+  (cond
+    (= current "do()") [true muls]
+    (= current "don't()") [false muls]
+    (true? save) [save (conj muls current)]
+    :else [save muls]))
 
 (defn part-2
   [filename]
@@ -31,10 +24,10 @@
     (slurp filename)
     (re-seq #"(mul\(\d{1,3},\d{1,3}\))|(do\(\))|(don't\(\))")
     (map #(% 0))
-    (collect-numbers)
+    (reduce save-muls [true []])
+    (->>nth 1)
     (map #(re-seq #"\d+" %))
-    (map to-int-each)
-    (map #(reduce * %))
+    (map (fn [[a b]] (* (str->int a) (str->int b))))
     (reduce +)))
 
 
